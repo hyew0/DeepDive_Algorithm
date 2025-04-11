95ec83982cac7bab9f1837679a1f8b563925a3cd
hw <lhy3899@gmail.com>
2025-04-11 16:15:08 +0900

    foundationSimulation

--- src/_05_FoundationSimulation/BangBang.java
+++ src/_05_FoundationSimulation/BangBang.java
@@ -12,14 +12,34 @@ public class BangBang {
         int n = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int BangCount =0;
+        long BangCount =0; // 총 발사 횟수
+        int count = 0;
 
-        for (int i = 1; i <= n; i++) {
+
+        for (int i = 0; i < n; i++) {
             int strength = Integer.parseInt(st.nextToken());
 
-            while (strength > 0) {
+            //권총이 주는 피해가 1이 될때까지, 체력이 남지 않을 때까지 반복
+            while (count > 0 && strength > 0) {
+                strength -= count + 1;
+                count = (count + 1) % 4;
                 BangCount++;
-                strength -=((BangCount - 1) % 4) + 1;
+            }
+
+
+            if (strength <= 0) {
+                continue;
+            }
+
+            //값이 10이 넘어갈경우 (1+2+3+4) = 10이므로 체력 10당 총을 4번 발사한 것임. 그래서 10으로 나눈 값에 4를 곱해주면 해당 횟수만큼 총을 쏜것.
+            BangCount += (strength / 10) *4;
+            strength %= 10;
+
+            //10미만의 체력이 남았다면 해당 횟수를 계산
+            while (strength > 0) {
+                strength -= count + 1;
+                count = (count + 1) % 4;
+                BangCount++;
             }
         }
 
